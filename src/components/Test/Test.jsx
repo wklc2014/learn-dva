056bcdf3bfd12a8ed477920cc0fb32b4799a6d49
wklc2014 <373658358@qq.com>
2017-08-23 20:00:05 +0800

    添加表格

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,156 +1,124 @@
 import React, { Component } from 'react';
-import { Table, Button } from 'antd';
+import { Table, Button, Input, InputNumber } from 'antd';
 import lodash from 'lodash';
+import { DATA_SOURCE, KEYS, COLUMNS, STATE_ADD } from './common/';
 
-const dataSource = [{
-    key: 'system',
-    type: '系统',
-    one: 3,
-    two: 12,
-    three: 24,
-    four: 24,
-    five: 24,
-    six: 24,
-    seven: 12,
-    eight: 3,
-}, {
-    key: 2,
-    type: '一号正常买法',
-    one: 5,
-    two: 2,
-    three: 1,
-    four: 1,
-    five: 1,
-    six: 1,
-    seven: 2,
-    eight: 5,
-}, {
-    key: 3,
-    type: '二号买法',
-    one: 5,
-    two: 1,
-    three: 1,
-    four: 1,
-    five: 1,
-    six: 1,
-    seven: 1,
-    eight: 5,
-}, {
-    key: 4,
-    type: '三号买法',
-    one: 5,
-    two: 2,
-    three: 1,
-    four: 1,
-    five: 1,
-    six: 1,
-    seven: 1,
-    eight: 5,
-}];
+const rate = 10 * 0.01;
 
 class Assess extends Component {
+
+    static defaultProps = {}
 
     constructor(props) {
         super(props);
         this.state = {
-            selectedRowKeys: [2],
-            dataSource: dataSource,
-            add: {
-                key: 'add',
-                type: '运算结果',
-                one: '',
-                two: '',
-                three: '',
-                four: '',
-                five: '',
-                six: '',
-                seven: '',
-                eight: '',
-            },
+            selectedRowKeys: [
+                'self-1',
+                'self-2',
+                'self-3',
+                'self-4',
+                'self-5',
+                'self-6',
+                'self-7',
+                'self-8',
+            ],
+            dataSource: DATA_SOURCE,
+            add: STATE_ADD,
         }
     }
 
-    getDataSource = () => {
-        const { add, dataSource } = this.state;
-        const newData = [...dataSource];
-        newData.push(add);
-        return newData;
+    handleChange = (value, record, key) => {
+        const { dataSource, selectedRowKeys } = this.state;
+        const newState = dataSource.map((v) => {
+            if (record.key === v.key) {
+                v[key] = value;
+            }
+            return v;
+        })
+        const data = this.getState(newState, selectedRowKeys);
+        this.setState(data);
     }
 
     onSelectChange = (selectedRowKeys) => {
-        this.setState({ selectedRowKeys });
+        const { dataSource } = this.state;
+        const data = this.getState(dataSource, selectedRowKeys);
+        this.setState(data);
+    }
+
+    getState = (dataSource, selectedRowKeys) => {
+        const { add } = this.state;
+        const newDataSource = dataSource.filter((v) => (lodash.indexOf(selectedRowKeys, v.key) !== -1 && (v.key + '').indexOf('system') === -1));
+        const system = dataSource.filter((v) => (lodash.indexOf(selectedRowKeys, v.key) !== -1 && v.key.indexOf('system') !== -1))[0] || {};
+        let xiaZhu = 0;
+        const kai = {};
+        KEYS.forEach((v) => {
+            kai[v] = 0;
+        })
+        newDataSource.forEach((m) => {
+            KEYS.forEach((v) => {
+                xiaZhu += m[v];
+                kai[v] += (system[v] || 0) * m[v];
+            })
+        })
+        KEYS.forEach((v) => {
+            let t = '无';
+            const s = xiaZhu - kai[v];
+            if (s > 0) {
+                t = '亏';
+            } else if (s < 0) {
+                t = '赢';
+            }
+            add[v] = (
+                <div>
+                    <p>{`开：${lodash.round(kai[v] * rate, 2)}万`}</p>
+                    <p>{`下：${lodash.round(xiaZhu * rate, 2)}万`}</p>
+                    <p>{`${t}：${lodash.round(Math.abs(s) * rate, 2)}万`}</p>
+                </div>
+            );
+        })
+        return { dataSource, add, selectedRowKeys }
     }
 
     handleClick = () => {
-        const { selectedRowKeys, add, dataSource } = this.state;
-        const keys = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-        // console.log('selectedRowKeys>>>', selectedRowKeys)
-        if (selectedRowKeys.length) {
-            const newDataSource = dataSource.filter((v) => lodash.indexOf(selectedRowKeys, v.key) !== -1);
-            const system = dataSource.filter((v) => v.key === 'system')[0];
-            let xiaZhu = 0;
-            const kai = {
-                one: 0,
-                two: 0,
-                three: 0,
-                four: 0,
-                five: 0,
-                six: 0,
-                seven: 0,
-                eight: 0,
-            };
-            newDataSource.forEach((m) => {
-                keys.forEach((v) => {
-                    xiaZhu += m[v];
-                    kai[v] += system[v] * m[v];
-                })
-            })
-            keys.forEach((v) => {
-                let t = '无';
-                const s = xiaZhu - kai[v];
-                if (s > 0) {
-                    t = '亏';
-                } else if (s < 0) {
-                    t = '赢';
-                }
-                add[v] = (
-                    <div>
-                        <p>{`下：${xiaZhu * 10}万`}</p>
-                        <p>{`开：${kai[v] * 10}万`}</p>
-                        <p>{`${t}：${Math.abs(s) * 10}万`}</p>
-                    </div>
-                );
-            })
-
-            this.setState({ add })
-        } else {
-            alert('请选择一行数据');
-        }
+        const { dataSource, selectedRowKeys } = this.state;
+        const data = this.getState(dataSource, selectedRowKeys);
+        this.setState(data);
     }
 
     render() {
-        const columns = [
-            { width: '10%', title: '买法', dataIndex: 'type', key: 'type' },
-            { width: '10%', title: 1, dataIndex: 'one', key: 'one' },
-            { width: '10%', title: 2, dataIndex: 'two', key: 'two' },
-            { width: '10%', title: 3, dataIndex: 'three', key: 'three' },
-            { width: '10%', title: 4, dataIndex: 'four', key: 'four' },
-            { width: '10%', title: 5, dataIndex: 'five', key: 'five' },
-            { width: '10%', title: 6, dataIndex: 'six', key: 'six' },
-            { width: '10%', title: 7, dataIndex: 'seven', key: 'seven' },
-            { width: '10%', title: 8, dataIndex: 'eight', key: 'eight' },
-        ]
-
-
-        const dataSource = this.getDataSource();
+        const { add, dataSource } = this.state;
+        const newDataSource = [...dataSource, add];
 
         const rowSelection = {
             selectedRowKeys: this.state.selectedRowKeys,
             onChange: this.onSelectChange,
             getCheckboxProps: (record) => {
-                return { disabled: (record.key === 'system' || record.key === 'add') }
+                return { disabled: false };
             }
         }
+
+        const columns = COLUMNS.map((v) => {
+            if (v.key !== 'type') {
+                v.render = (text, record, index) => {
+                    const dom = (
+                        <InputNumber
+                            value={text}
+                            min={0}
+                            step={1}
+                            onChange={(e) => { this.handleChange(e, record, v.key); }}
+                        />
+                    )
+                    if (record.key.indexOf('type-') !== -1) {
+                        // return dom;
+                        return `${text * 10}万`;
+                    } else if (record.key.indexOf('self-') !== -1) {
+                        return dom;
+                    }
+                    return text;
+                }
+            }
+            return v;
+        });
 
         return (
             <div>
@@ -163,9 +131,10 @@ class Assess extends Component {
                     rowKey="key"
                     pagination={false}
                     columns={columns}
-                    dataSource={dataSource}
+                    dataSource={newDataSource}
                     bordered
                     rowSelection={rowSelection}
+                    size="small"
                 />
             </div>
         )
